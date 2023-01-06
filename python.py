3aa9d4e51aaf39ad494efdad7f59e88d41937c22
Abhishek <ak8888780@gmail.com>
2023-01-06 11:54:07 +0530

    this is a simple calculator made by me am newbie in open source

---
+++ python.py
@@ -0,0 +1,9 @@
+a = 6
+b = 1
+ 
+print("The addition of a and b is :",a+b)
+print("The subtraction of a and b is :",a-b)
+print("The Multiplication of a and b is :",a*b)
+print("The divident of a and b is :",a/b)
+ 
+
